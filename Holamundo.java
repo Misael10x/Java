1280ef3dd22072cd02c99ca2ff794b40873e46bd
Misael10x <91623505+Misael10x@users.noreply.github.com>
2021-10-05 22:49:20 -0500

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,20 @@
+public class Holamundo{
+	/**
+	 * Aplicacion java
+	 * Title	:Hola mundo en java 
+	 * Descripcion	:Hola mundo en java desde vim
+	 * Author	:Misael Barbosa Arce
+	 * Date		:20211005
+	 * Version	:1
+	 * Usage	:C:\Users\MASTER\Documents\HolaJava
+	 *
+	 * Hola mundo en java
+	 *
+	 */
+
+public static void main(String []args){
+	System.out.println("Hola mundo en java");//prints Holamundo
+
+	}
+}
+
